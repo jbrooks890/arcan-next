4d5dbd8c8b8eb1f2106693809054a81d18fed02d
ArcanBoi <61268478+ArcanBoi@users.noreply.github.com>
2023-03-21 16:53:07 -0400

    minors

--- src/components/pages/read/Contents.tsx
+++ src/components/pages/read/Contents.tsx
@@ -5,6 +5,10 @@ export default function Contents({ content }: { content?: string }) {
     "Turbulent Calm; Corruption; Living Darkness; Faces of Truth; The Company Kept; Scheme of the Wicked; Pursuit; Nexus Advent; Closed Eyes Open; The Descended; Trouble In Paradise; The Powers That Be; The Deadwoods; The Wick; Rough Acquaintances; Unshadowed; Restless Outsiders; Obstruction & Injustice; Inky Truth; Under the Black Sun; The Red Dragon; Seized Souls; Revenant; Near & Far".split(
       "; "
     );
+
+  const _UNAVAILABLE = Math.floor(Math.random() * CHAPTERS.length);
+  console.log({ _UNAVAILABLE });
+
   return (
     <div className={`${styles.wrapper} contents`}>
       <h3>Contents</h3>
@@ -15,12 +19,14 @@ export default function Contents({ content }: { content?: string }) {
             <button
               key={i}
               className={`${styles.button} ${
-                _PROGRESS <= 0
+                i > _UNAVAILABLE
+                  ? "unavailable"
+                  : _PROGRESS <= 0
                   ? "not-started"
                   : _PROGRESS >= 100
                   ? "complete"
                   : "in-progress"
-              }`}
+              } `}
               data-chapter-progress={_PROGRESS}
               tabIndex={0}
             >
